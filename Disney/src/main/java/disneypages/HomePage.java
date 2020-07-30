04d62e27d9ed3a73d415cfd32d0b41bd2a0433fe
angelsuazo <angel.jose.suazo@gmail.com>
2020-07-30 11:34:01 -0400

    first commit

--- Disney/src/main/java/disneypages/HomePage.java
+++ Disney/src/main/java/disneypages/HomePage.java
@@ -1,26 +1,67 @@
 package disneypages;
 
-public class HomePage {
+import Base.CommonAPI;
+import datasources.ConnectToExcelFiles;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.BeforeMethod;
 
-//Start from HomePage add the WebElement(s) for the page with the methods of those elements. Add more pages as your
-//module needs along the way like Mafi bhai. Try to connect one page to the next as well.
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
-    //Very important:
-    //1) Go to every module and change the OS if your mac to "OS X", if windows to "Windows" under the
-    // runner.xml file.
-    //2) For mac users the driver path in CommonAPI works with one dot, for Windows it works with 2 dots(Windows can
-    //leave as is since I'm Windows. Mac change to one dot. Also you need to go to the connectoToSQLDatabase
-    //class and also change to one dot for the path for mac users. For windows its set to two already, no worries.
-    //The chromedriver is for chrome 84 if you got compatiblity issues follow and get the correct
-    // browser-to-driver compatibility.
-    //3) Under secret.properties file put your mySQL credentials: user, password and name of your database.
-    //4) Continue to build your pages and connect some to others, test with assertions under AllFunctionality
-    // class(just
-    //add to it). Once done run the xmlrunner.
-    //5)For mysql you can have compatibility issues, make sure if you have mySQL 8.0 you havew the connector jar to
-    // 8.0 as well,
-    //if you clone this it should be good.
-    //6)Ill fix up the database part and excel. But for database to retrieve from database you need to make your
-    //own methods based on the object you are trying to get if any.
+
+public class HomePage extends CommonAPI {
+
+    @FindBy(xpath = "//iframe[contains(@id,'google_ads')]") public static WebElement promotionIframe;
+    @FindBy(xpath = "//span[@id = 'goc-bar-left']/li[contains(@class, 'bar-dropdown')]") public static List<WebElement> allDropDownMenu;
+    @FindBy(id ="goc-instant-search-input") public static WebElement searchBar;
+    //*[@id="overlaybg"]/p/a
+
+    @BeforeMethod
+    public void closePromotionWindow() {
+        WebElement frame = driver.findElement(By.xpath("//iframe[contains(@id,'google_ads')]"));
+        driver.switchTo().frame(frame);
+
+        WebElement closeOverlayButton = driver.findElement(By.xpath("//a[contains(@class, 'sprite close')]"));
+        closeOverlayButton.click();
+        driver.switchTo().defaultContent();
+    }
+
+    public void search(String typedValueInSearch) {
+        searchBar.click();
+        typeOnElement(searchBar,typedValueInSearch);
+    }
+
+
+    public List getAllDropDownMenu(){
+        return getListOfStringText(allDropDownMenu,"Drop-Down");
+    }
+
+    public void getNumberOfLinks(){
+        numberOfLinks();
+    }
+
+    public String getTitle(WebDriver driver){
+        return driver.getTitle();
+    }
+
+    public void searchItemsWithExcelData(){
+        List<String> items;
+        try {
+             items = ConnectToExcelFiles.getItems();
+            for (String item : items
+                 ) {
+                System.out.println(item);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
+
+
